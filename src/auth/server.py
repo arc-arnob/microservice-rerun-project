0cd586b63a71fed4cc25dafa20030e706bcd0749
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-11-14 09:16:46 +0530

    Inital setup for Python based microservices

---
+++ src/auth/server.py
@@ -0,0 +1,13 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+#config
+
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+
+print(server.config["MYSQL_HOST"])
